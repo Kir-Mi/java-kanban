8ec2b0313cb7ba98cd225c451634e854781ee617
Kir <you_can_more@mail.ru>
2023-01-18 13:50:40 +0300

    refactor: переделать метод add

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -1,21 +1,15 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    int numberOfViews = 10; // количество сохраняемых просмотров
-    List<Task> viewsHistory = new ArrayList<>(numberOfViews); // список последний просмотров
+    private final int numberOfViews = 10; // количество сохраняемых просмотров
+    LinkedList<Task> viewsHistory = new LinkedList<>(); // список последних просмотров
 
     @Override
     public void add(Task task) {
-        if (viewsHistory.size() < numberOfViews) { // проверяем заполнен ли список
-            viewsHistory.add(task);
-        } else {
-            ArrayList<Task> copyViewsHistory = new ArrayList<>(viewsHistory);
-            viewsHistory.clear();
-            for (int i = 1; i < copyViewsHistory.size(); i++) {
-                viewsHistory.add(copyViewsHistory.get(i));
-            }
-            viewsHistory.add(task);
+        viewsHistory.add(task);
+        if (viewsHistory.size() > numberOfViews) {
+            viewsHistory.removeFirst();
         }
     }
 
